e89d0e9554ccc43982bd1b1aca6f09e5768be590
silverlass <silver.lass@khk.ee>
2019-11-12 15:53:25 +0200

    document changes

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-// single element selector
+sisu = document.querySelector('#title'); // id järgi
+sisu = document.querySelector('.card-title'); // klassnime järgi
+sisu = document.querySelector('h5'); // elemendi järgi
 
-sisu = document.getElementById('title');
+// stiili muutmine
 
-id = document.getElementById('title').id; // id
-klass = document.getElementById('title').className;
-
-
-document.getElementById('title').style.background = '#333';
-
-sisu.style.color = '#fff';
-sisu.style.padding = '5ps';
-sisu.textContent = 'Minu ülesanded';
-sisu.innerText = 'Ainult minu ülesanded';
-sisu.innerHTML = '<span style="color:green">Vabad ülesanded</span>';
-
-
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue';
+document.querySelector('li:last-child').style.color = 'red';
+document.querySelector('li:nth-child(2)').style.color = 'orange';
 
 console.log(sisu);
