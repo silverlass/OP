04f97269cc106ce69cbdd5279bd1ae2d8e6bf35f
silverlass <silver.lass@khk.ee>
2019-11-12 15:33:53 +0200

    Stiili muutmine domis

--- app.js
+++ app.js
@@ -1,44 +1,16 @@
-// document
+// single element selector
 
-let sisu;
+sisu = document.getElementById('title');
 
-sisu = document; //sisu html-is
-sisu = document.all; // html tagid kasutamise järjekorras
+id = document.getElementById('title').id; // id
+klass = document.getElementById('title').className;
 
-sisu = document.all[2]; // konkreetne html tag
-sisu = document.all.length; // kui palju on html tagi
 
-//document properties 
-sisu = document.head; // head isu
-sisu = document.body; // body sisu
-sisu = document.doctype; // doctype
-sisu = document.domain; // domeen
-sisu = document.URL; //URL
-sisu = document.characterSet;
-sisu = document.contentType;
+document.getElementById('title').style.background = '#333';
 
-// formi andmed
-sisu = document.forms; // kõik vormid
-sisu = document.forms[0]; //esimene form
-sisu = document.forms[0].id; // esimese vormi ID
-sisu = document.forms[0].method; // vormi töötlus
-sisu = document.forms[0].action;
+sisu.style.color = '#fff';
+sisu.style.padding = '5ps';
 
-//linkide andmed
-sisu = document.links; // kõik lingid
-sisu = document.links[0]; // esimene link
-sisu = document.links[0].className; // lingi klassid
-sisu = document.links[0].classList[0]; // klasside massiiv ja sealt elementide lugemine
-
-//pildid
-
-sisu = document.images;
-
-// skriptid
-
-sisu = document.scripts;
-sisu = document.scripts[1].getAttribute('src'); // lokaalne skript
-sisu = document.scripts[0].getAttribute('src'); // materialize skript
 
 
 console.log(sisu);
