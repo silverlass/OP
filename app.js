d53ef8390e2c21013f1ec44cc0ffaa4e106a5edb
silverlass <silver.lass@khk.ee>
2019-11-15 13:07:24 +0200

    Mitmese elemendi töötlused, massiivi läbimine tsükliga

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ let sisu = document.getElementsByClassName('collection-item');
 console.log(sisu);
 console.log(sisu[0]);
 
-sisu[0].style.color = 'orange';
-sisu[2].innerHTML = 'Õpi arvutivõrgud';
+sisu[0].style.color = 'red';
+
 
 
 // queryselector + klassnime järgi
@@ -19,7 +19,28 @@ let nimekirjaElemendid = document.getElementsByTagName('li');
 // teisendame HTMLcollection massiiviks (Array)
 
 nimekirjaElemendid = Array.from(nimekirjaElemendid);
-nimekirjaElemendid.reverse();
+//nimekirjaElemendid.reverse();
+
+// massiivi läbimine tüskliga
+
+for(let i=0;i<nimekirjaElemendid.length;i++){
+    nimekirjaElemendid[i].style.background = 'blue';
+    nimekirjaElemendid[1].style.background = 'black';
+    nimekirjaElemendid[2].style.background = 'white';
+    
+    nimekirjaElemendid[i].style.color = 'pink';
+}
+//for each
+
+/*
+nimekirjaElemendid.forEach(function(element){
+  console.log(element);
+  element.style.background = "#ddd"
+})
 
 
 console.log(nimekirjaElemendid)
+*/
+
+let nimekiriPaaritu = document.querySelectorAll('li:nth-child(even');
+console.log(nimekiriPaaritu);
