dfb04367d1cd3dfb35863ba5549db94b60ca0991
silverlass <silver.lass@khk.ee>
2019-11-18 11:26:30 +0200

    ul childi li kustutamine

--- app.js
+++ app.js
@@ -1,40 +1,20 @@
-// lea vana element h5
-
-const vanaPealkiri = document.querySelector('h5');
-
-// leia kaart, kus antud pealkiri on
-
-const divCardAction = document.querySelector('.card-action');
-
-// loo uus pealkiri
-
-const uusPealkiri = document.createElement('h3');
-
-uusPealkiri.id = 'new-title';
-
-// lisa sisu
-
-uusPealkiri.textContent = 'Uued ülesanded';
-
-//uusPealkiri.appendChild(document.createTextNode('Uued ülesanded'));
-
-// asenda vana pealkiri uuega
-
-divCardAction.replaceChild(uusPealkiri, vanaPealkiri);
 
 
-console.log(uusPealkiri);
 
+const liElemendid = document.querySelectorAll('li');
 
-const vana = document.querySelector('span');
+// kustuta konkreetne element
 
-console.log(vana);
+liElemendid[2].remove();
 
-const divVana = document.querySelector('.card-content');
+// leia antud DOMis ul element
 
-const uus = document.createElement('h3');
+const ulElement = document.querySelector('ul');
 
-uus.textContent = "Ülesanded";
+//kustuta antud elemendist li
+// li on ul child element
 
-divVana.replaceChild(uus, vana);
+ulElement.removeChild(liElemendid[1]);
+
+console.log(ulElement);
 
