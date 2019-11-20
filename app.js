a0677bc022250a5a7d79f836b19645871f3ee179
silverlass <silver.lass@khk.ee>
2019-11-20 09:24:00 +0200

    Event objekti omadused

--- app.js
+++ app.js
@@ -3,11 +3,34 @@
 
 const kustutaKoik = document.querySelector('.clear-tasks');
 
-kustutaKoik.addEventListener('click', vajutaNupp() );
+kustutaKoik.addEventListener('click', vajutaNupp);
 
 
 function vajutaNupp(e){
-console.log('vajutatud');
-    // e.preventDefault();
+let sisu = e;
+
+// target
+
+sisu = e.target;
+sisu = e.target.id;
+sisu = e.target.className;
+sisu = e.target.classList;
+
+// tüüp
+
+sisu = e.type;
+
+//sündmuse kooridnaadid browseri akna suhtes
+
+sisu = e.clientY;
+sisu = e.clientX;
+
+//sündmuse kooridnaadid elemendi enda suhtes
+
+sisu = e.offsetY;
+sisu = e.offsetX;
+
+console.log(sisu);
 
 }
+
