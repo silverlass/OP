c7630f9268ed2319bb61ea3ad0b51ebde8622171
silverlass <silver.lass@khk.ee>
2019-11-20 11:54:52 +0200

    localstorage

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
-// sündmused
+// // storage
 
-// bubbling
+// // salvesta väärtus
 
-// lehe kaardi pealkiri
-// document.querySelector('.card-title').addEventListener('click', function logi(e){
-//    console.log('.card-title');
+// localStorage.setItem('nimi', 'Silver');
+// localStorage.setItem('perenimi', 'Lass');
 
-// });
+// // väärtuse eemaldamine
 
-// document.querySelector('.card-content').addEventListener('click', function(e){
-//    console.log('.card-content');
+// //localStorage.removeItem('nimi', 'Silver');
 
-// });
+// // väärtuste lugemine
+// const nimi = localStorage.getItem('nimi');
+// console.log(nimi);
 
-const kustutaYlesanne = document.querySelector('.delete-item');
-kustutaYlesanne.addEventListener('click', kustuta);
-function kustuta(e){
-    console.log('hakkame kustutama');
-    console.log(e.target.parentElement.parentElement);
-    if(e.target.parentElement.classList.contains('delete-item')){
-    e.target.parentElement.parentElement.remove();
+// // eemalda kõik väärtused
+
+// localStorage.clear();
+
+document.querySelector('form').addEventListener('submit', salvesta);
+
+function salvesta(e){
+    const uusYlesanne = document.getElementById('task').value;
+    localStorage.setItem('tasks', uusYlesanne);
+
+    let ylesanded;
+    if (localStorage.getItem('tasks') === null){
+        ylesanded = [];
+        console.log(ylesanded);
+    }else{
+        console.log('olemas')
+        
+    }
+
+
+
+    localStorage.setItem('tasks', uusYlesanne);
 }
-}
-
-
-console.log(kustutaYlesanne);
-
