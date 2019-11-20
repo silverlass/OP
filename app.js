4e76bb8f9aa999d8a96f79c8c09d675760613ad3
silverlass <silver.lass@khk.ee>
2019-11-20 10:29:03 +0200

    sündmused - vormi saatmine

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 
 // events
 
-const kustutaKoik = document.querySelector('.clear-tasks');
+const form = document.querySelector('form');
+const lisaUusYlesanne = document.getElementById('task');
 
 const text = document.querySelector('h5');
 const kaart = document.querySelector('.card');
 
-//kustutaKoik.addEventListener('click', syndmus);
-//kustutaKoik.addEventListener('dblclick', syndmus);
-//kustutaKoik.addEventListener('mousedown', syndmus);
-//kaart.addEventListener('mouseleave', syndmus);
-//kaart.addEventListener('mouseneter', syndmus);
-//kaart.addEventListener('mouseover', syndmus);
-//kaart.addEventListener('mouseleave', syndmus);
+// kustutame lisaUusYlesanne väärtus
+
+lisaUusYlesanne.value='';
+
+
+//vormi saatmine
+
+form.addEventListener('submit', syndmus)
 
 function syndmus(e){
     // logi sündmuse tyyp
 
     console.log(`sündmuse tyyp: ${e.type}`);
+    // kontrollime vormi kaudu saadud väärtust
+    
+    console.log(lisaUusYlesanne.value);
+    // väljastame ülesande html-is
 
-    // muuda text sisu: Hiirx kooridnaat, HiirY koordinaat
-
-    text.textContent = "HiirX: " + e.offsetX + " HiirY: " + e.offsetY + ' Jüri IQ on:  ' + e.offsetX;
-
-    // body värv
-
-    document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`
+    text.innerText = lisaUusYlesanne.value;
 }
 
