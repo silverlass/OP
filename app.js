8278f4d64bdc97ae665ec46d7ae7895e1c93ad23
silverlass <silver.lass@khk.ee>
2020-01-20 10:44:54 +0200

    Todo list with LS Ready

--- app.js
+++ app.js
@@ -1,71 +1,149 @@
 
 
 class Ylesanne{
-    constructor(sisu){
-    this.sisu = sisu;
+    constructor(y){
+    this.task = y;
   }
 };
 
+
+
 class KL{
+  
 
-    
+  addTask(y){
+      const rida = document.createElement('li');
+      rida.innerHTML = `
+      <p>${y.task}</p>
+      <a class="delete-item secondary-content"><i class="fa kustuta fa-remove"></i></a>`;
 
-    lisaYlesanne(){
-    let todosUl = document.querySelector('ul');
-    let todosLi = document.createElement('li');
-    let a = document.createElement('a');
-    let i = document.createElement('i');
-    
-    todosLi.innerHTML = document.getElementById('task').value;
-    
-    todosLi.className = "collection-item";
-    a.className = "secondary-content delete-item";
-    a.href = "#";
-    i.className = "fa fa-remove";
-    
-    todosUl.appendChild(todosLi);
-    todosLi.appendChild(a);
-    a.appendChild(i);
-    let as  = document.querySelectorAll('.delete-item');
-    for (let element of as){
-        element.addEventListener('click', function(){element.parentNode.remove();
-        })
+      const tabel = document.querySelector('.collection');
+      tabel.appendChild(rida);
+  };
+  clearTask(){
+    document.getElementById('task').value = '';
+  };
+
+
+
+  deleteTask(kustutaElement){
+      if(kustutaElement.className === 'fa kustuta fa-remove'){
+          const tabelRow = kustutaElement.parentElement.parentElement;
+          tabelRow.remove();
+          return true;
       }
-    }
-    
-    kustutaKoik(){
-      document.querySelector('.collection').innerHTML = '';
-    }
+  };
+
+  showTasks(){
+      const ls = new LS();
+      const TheTasks = ls.getTasks();
+      TheTasks.forEach(function(ylesanne){
+          const y = new Ylesanne(ylesanne['task']);
+
+          const kl = new KL();
+
+          kl.addTask(y)
+      })
+  }
 }
+
+
+document.addEventListener('DOMContentLoaded', TheTasks);
+
+function TheTasks(e){
+    const kl = new KL();
+
+    kl.showTasks();
+};
+
+document.getElementById('clear-tasks').addEventListener('click', clearTasks);
+
+function clearTasks(e){
+    const tasksList = document.querySelector('.collection');
+    while(tasksList.firstChild) {
+        tasksList.removeChild(tasksList.firstChild);
+    }
+
+    localStorage.clear();
+    const kl = new KL();
     
-      
+}
 
+document.getElementById('add-task').addEventListener('submit', addTheTask);
 
-let lisa = new KL();
-let eemaldaKoik = new KL();
+function addTheTask(e){
+    const task = document.getElementById('task').value;
 
+    const ylesanne = new Ylesanne(task);
 
-document.querySelector('.btn_lisaylesanne').addEventListener('click', lisa.lisaYlesanne);
-document.querySelector('.clear-tasks').addEventListener('click', eemaldaKoik.kustutaKoik);
+    const kl = new KL();
 
+    
+        kl.addTask(ylesanne)
+
+        const ls = new LS();
+
+        ls.saveTask(ylesanne);
+        
+    
+
+    kl.clearTask();
+    e.preventDefault();
+};
+
+document.querySelector('.collection').addEventListener('click', deleteTheTask);
+
+function deleteTheTask(e){
+    const kl = new KL();
+
+    const X = e.target
+
+    task = X.parentElement.previousElementSibling.textContent;
+    kl.deleteTask(X)
+
+    const ls = new LS();
+
+    isDeleted = ls.deleteTaskLS(task);
+
+    if(isDeleted){
+        
+       }
+       
+       e.preventDefault();
+     }
+    
 
 
 class LS{
-  lisaLocalStoragisse(){
-  // Plaanime luua localstoragisse lisamise  
-    
-    localStorage.setItem('todo',JSON.stringify(document.getElementById('task').value));
-    
-  }
+  getTasks(){
+      let TheTasks;
 
-  votaLocalStoragist(){
-    // Localstoragist lugemine
-    localStorage.getItem
-  }
+      if(localStorage.getItem('TheTasks' ) === null){
+          TheTasks = [];
+      } else {
+          TheTasks = JSON.parse(localStorage.getItem('TheTasks'));
+      }
+      return TheTasks;
+  };
+  
+  saveTask(y){
+      const TheTasks = this.getTasks();
+      TheTasks.push(y);
 
-  //Localstoragist kustutamine
+      localStorage.setItem('TheTasks', JSON.stringify(TheTasks));
+  };
+
+  deleteTaskLS(task){
+      const TheTasks = this.getTasks();
+      TheTasks.forEach(function(ylesanne, index){
+
+          if(ylesanne.task === task){
+              TheTasks.splice(index, 1);
+          }
+      });
+      localStorage.setItem('TheTasks', JSON.stringify(TheTasks));
+      return true;
+  }
 }
-let lisaLS = new LS()
-document.querySelector('.btn_lisaylesanne').addEventListener('click', lisaLS.lisaLocalStoragisse);
 
 
